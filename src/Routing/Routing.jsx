c61c68d6b2e54c6d25c738ec6f83c831e6350e01
Krystsina Vouk <krystsina.vouk@empirica.io>
2022-05-16 01:37:53 +0200

    Lazy loading [implemented]

--- src/Routing/Routing.jsx
+++ src/Routing/Routing.jsx
@@ -1,20 +1,24 @@
 import { Redirect, Route, Switch } from "react-router-dom";
-import MainPage from "../Pages/MainPage/MainPage";
-import AboutPage from "../Pages/AboutPage/AboutPage";
-import VotesPage from "../Pages/VotesPage/VotesPage";
+import React from "react";
 import CreateVotePage from "../Pages/CreateVotePage/CreateVotePage";
-import CatVotePage from "../Pages/CatVotePage/CatVotePage";
+
+const LazyMainPage = React.lazy(() => import("../Pages/MainPage/MainPage"));
+const LazyAboutPage = React.lazy(() => import("../Pages/AboutPage/AboutPage"));
+const LazyVotesPage = React.lazy(() => import("../Pages/VotesPage/VotesPage"));
+const LazyCatVotePage = React.lazy(() => import("../Pages/CatVotePage/CatVotePage"));
 
 
 export const Routing = () => {
     return (
-        <Switch>
-            <Route component={MainPage} path={"/"} exact={true} />
-            <Route component={AboutPage} path={"/about"} exact={true} />
-            <Route component={VotesPage} path={"/about/votes"} exact={true} />
-            <Route component={CreateVotePage} path={"/about/create-vote"} exact={true} />
-            <Route component={CatVotePage} path={"/about/votes/:id"} />
-            <Redirect to={"/"} />
-        </Switch>
-    )
+        <React.Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+                <Route component={LazyMainPage} path={"/"} exact={true} />
+                <Route component={LazyAboutPage} path={"/about"} exact={true} />
+                <Route component={LazyVotesPage} path={"/about/votes"} exact={true} />
+                <Route component={CreateVotePage} path={"/about/create-vote"} exact={true} />
+                <Route component={LazyCatVotePage} path={"/about/votes/:id"} />
+                <Redirect to={"/"} />
+            </Switch>
+        </React.Suspense>
+    );
 }
